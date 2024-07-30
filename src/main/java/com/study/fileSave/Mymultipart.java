f4ff1b37b2daeffa66ee3b4aaa2bcc34b4f68122
lee93 <dohuny2@naver.com>
2024-07-30 11:04:43 +0900

    Revert "branch test"
    This reverts commit 19d0634bb297a2e1e9192f8a78998db5c4c48037.

--- src/main/java/com/study/fileSave/Mymultipart.java
+++ src/main/java/com/study/fileSave/Mymultipart.java
@@ -4,38 +4,33 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Mymultipart {
-
     public void readFile(File file) {
         String boundary = "";
         try {
             FileReader fr = new FileReader(file);
             BufferedReader br = new BufferedReader(fr);
-
-            List<String> contents = new ArrayList<String>();
-
+            String header = "";
+            String content = "";
             while(true){
                 String line = br.readLine();
-                String content = "";
                 boolean capture = false;
                 // boundary 추출
                 if(line.contains("boundary")){
                     boundary = line.substring(line.indexOf("=")+1);
                     System.out.println(boundary);
                 }
-                // boundary 단위로 추출
                 if(line.contains("--"+boundary)){
                     System.out.println(line);
                     capture = true;
                 }
                 if(capture){
-                    content += line;
-                    if(line.contains("--"+boundary)){
-                        capture = false;
+                    header += line;
+                    if (line.isEmpty()) {
+                        content += line;
                     }
+                    capture = false;
                 }
             }
 
@@ -43,9 +38,5 @@ public class Mymultipart {
         } catch (Exception e) {
             e.printStackTrace();
         }
-    }
-
-    public void fileData(){
-
     }
 }
